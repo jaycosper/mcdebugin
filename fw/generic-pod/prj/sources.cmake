0c4663db443343a3eb0140f4ce976fa0ef75c63c
Jay Cosper <jaycosper@gmail.com>
2026-03-26 21:53:01 -0700

    adding 8b10b decoder/encoder from repo

--- fw/generic-pod/prj/sources.cmake
+++ fw/generic-pod/prj/sources.cmake
@@ -1,7 +1,9 @@
 # Add each library's CMake file
 
 include(${IP_DIR}/ip.cmake)
-include(${PROJECT_TOP_DIR}/src/top.cmake)
-include(${MODULES_DIR}/protocol_lvds/protocol_lvds.cmake)
-include(${MODULES_DIR}/lvds_rxtx/lvds_rxtx.cmake)
+
 include(${MODULES_DIR}/debounce/debounce.cmake)
+include(${MODULES_DIR}/lvds_rxtx/lvds_rxtx.cmake)
+include(${MODULES_DIR}/protocol_lvds/protocol_lvds.cmake)
+include(${MODULES_DIR}/v8b10b/v8b10b.cmake)
+include(${PROJECT_TOP_DIR}/src/top.cmake)
